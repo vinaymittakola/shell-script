a4c209292d770a738de465cf88e07a7bc3620438
vinaymittakola <mittakolavinay1@gmail.com>
2024-04-17 17:03:06 +0530

    hello script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "please enter username:"
-read -s USERNAME
+read USERNAME   #read -s USERNAME if name is not visible 
 echo "please enter password:"
 read -s PASSWORD
 echo "your username is $USERNAME AND password is $PASSWORD"
---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+movie=("RRR","KGF","saho")
+
+# size of the above array is 3
+# index is 2 ...starts with 0 1 2...
+
+echo "1st movie is ${movie}"
