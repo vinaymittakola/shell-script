35dbdd61534bba347117864ceff58c5117ac0b24
vinaymittakola <mittakolavinay1@gmail.com>
2024-04-17 17:05:09 +0530

    hello script

--- 06-array.sh
+++ 06-array.sh
@@ -5,4 +5,10 @@ movie=("RRR","KGF","saho")
 # size of the above array is 3
 # index is 2 ...starts with 0 1 2...
 
-echo "1st movie is ${movie}"
+echo "1st movie is ${movie[0]}"
+
+echo "2nd movie is ${movie[1]}"
+
+echo "3rd movie is ${movie[2]}"
+
+echo "movies are ${movie[@]}"
