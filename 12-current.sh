d20e4f71f1915f6845ffa2d0f45a7af32a1ac839
vinaymittakola <mittakolavinay1@gmail.com>
2024-08-24 17:15:04 +0530

    functions

---
+++ 12-current.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+var="hello how are you"
+
+echo "Iam good , question $var"
+echo "process id $$"
+
+./13-other.sh 
+
+echo "Iam not well , question $var"
+echo "process id $$"
---
+++ 13-other.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+course="from devops team"
+
+echo "aws certification $course"
+echo "PID $$"
+ 
+
+
